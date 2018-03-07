a58bca43c383347f99978b41e80e616b0f333f00
David R. Piegdon <dgit@piegdon.de>
2018-03-07 20:54:32 +0000

    allow successive processing of multiple paths

--- segmentation.py
+++ segmentation.py
@@ -256,15 +256,30 @@ commands = collections.OrderedDict([
 ])
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3 or sys.argv[1] not in commands:
-        print("required arguments: <command> <path>")
+    if len(sys.argv) < 3 or sys.argv[1] not in commands:
+        print("required arguments: <command> <path> [path [...] ]")
         print("where command is one of:")
         for c in commands:
             print("  {} -- {}".format(c, commands[c][1]))
         sys.exit(1)
 
-    task = commands[sys.argv[1]][0](sys.argv[2])
-    ret = task.run()
-    print("task '{}' finished with result '{}'".format(sys.argv[1], ret))
+    taskname = sys.argv[1]
+    paths = sys.argv[2:]
+
+    print("processing files in these paths:")
+    for path in paths:
+        print("  " + path)
+
+    rets = []
+    for path in paths:
+        task = commands[taskname][0](path)
+        ret = task.run()
+        print("task '{}' for path '{}' finished with result '{}'".format(taskname, path, ret))
+        rets.append(ret)
+
+    print("overall result for task '{}':".format(taskname))
+    for path, ret in zip(paths, rets):
+        print("  path {}: {}".format(path, "ok" if ret == 0 else "error"))
+    ret = sum(rets)
     sys.exit(ret)
 
