bdcf59fbb6286342d2e644ca040c7ac8c8f0ba65
David R. Piegdon <dgit@piegdon.de>
2018-07-15 17:27:30 +0000

    stick to uint8 or boolean where possible

--- assets/apply_models.py
+++ assets/apply_models.py
@@ -58,26 +58,27 @@ def _calc_model_likelihoods(shape, points10d, models):
 
 _color_index_palette = [
     # segment 0 (black) is reserved and should never be used
-    np.array([0.,0.,0.]), # black
-    np.array([1.,0.,0.]), # red
-    np.array([0.,1.,0.]), # green
-    np.array([0.,0.,1.]), # blue
-    np.array([0.,1.,1.]), # cyan
-    np.array([1.,0.,1.]), # magenta
-    np.array([1.,1.,0.]), # yellow
-    np.array([1.,1.,1.]), # white
+    np.array([000,000,000], dtype=np.uint8), # black
+    np.array([255,000,000], dtype=np.uint8), # red
+    np.array([000,255,000], dtype=np.uint8), # green
+    np.array([000,000,255], dtype=np.uint8), # blue
+    np.array([000,255,255], dtype=np.uint8), # cyan
+    np.array([255,000,255], dtype=np.uint8), # magenta
+    np.array([255,255,000], dtype=np.uint8), # yellow
+    np.array([255,255,255], dtype=np.uint8), # white
 ]
 
 def _image_indexed2color(image_xy):
-    result = np.zeros([image_xy.shape[0], image_xy.shape[1], 3])
-    done = np.zeros([image_xy.shape[0], image_xy.shape[1]])
+    # returns image in uint8 format
+    result = np.zeros([image_xy.shape[0], image_xy.shape[1], 3], dtype=np.uint8)
+    done = np.zeros([image_xy.shape[0], image_xy.shape[1]], dtype=np.bool)
     for i, color in enumerate(_color_index_palette, start=0):
         mask = (image_xy == i)
         result[mask] = color
-        done[mask] = 1;
+        done[mask] = True;
 
     pixels = image_xy.shape[0] * image_xy.shape[1]
-    pixels_done = int(np.sum(done))
+    pixels_done = np.sum(done)
     if(pixels_done != pixels):
         raise ValueError("Not all pixels were assigned to an index ({}/{}). ".format(pixels_done, pixels)
                     +"Maybe too many segments? Extend _color_index_palette!")
@@ -86,11 +87,12 @@ def _image_indexed2color(image_xy):
 def _image_color2indexed(image_xyc, expected_segments):
     # XXX this is kind of messy. found no better way yet.
     # segment 0 (black) is reserved and should never be used
+    # expects image in uint8 format
     if image_xyc.shape[2] != 3:
         raise ValueError("Manually edited segmentation image has invalid format. Did you add an alpha channel?")
     shape = [image_xyc.shape[0], image_xyc.shape[1]]
-    result = np.zeros(shape)
-    done = np.zeros(shape)
+    result = np.zeros(shape, dtype=np.uint8)
+    done = np.zeros(shape, dtype=np.bool)
     for x in range(shape[0]):
         for y in range(shape[1]):
             for i, color in enumerate(_color_index_palette, start=0):
@@ -98,11 +100,11 @@ def _image_color2indexed(image_xyc, expected_segments):
                     break
                 if np.all(color == image_xyc[x,y]):
                     result[x,y] = i
-                    done[x,y] = 1
+                    done[x,y] = True
                     break
 
     pixels = shape[0] * shape[1]
-    pixels_done = int(np.sum(done))
+    pixels_done = np.sum(done)
     if(pixels_done != pixels):
         raise ValueError("Not all pixels could be assigned to an index ({}/{}). ".format(pixels_done, pixels)
                     +"Did you edit such that only index-colors are contained in image?")
@@ -113,7 +115,7 @@ def _segment_file(image_file):
     return str(image_file).replace("_preprocessed.png", "_segmentation.png")
 
 def _calc_model_segmentation(image_file, shape, models, likelihoods, model2id, min_object_size=75):
-    segmentation = np.zeros(shape)
+    segmentation = np.zeros(shape, dtype=np.uint8)
     for m in models:
         modelmask = np.ones(shape, dtype=bool)
         for other in models:
@@ -143,7 +145,7 @@ def _calc_model_segmentation(image_file, shape, models, likelihoods, model2id, m
 def _load_model_segmentation(image_file, shape, models, min_object_size=75):
     try:
         segmentation = _image_color2indexed(
-                            img_as_float(io.imread(_segment_file(image_file))),
+                            io.imread(_segment_file(image_file)),
                             len(models)
                         )
     except ValueError:
@@ -178,7 +180,7 @@ def _plot_model_segments(image_file, rgb_image, models, model2id, segmentation, 
     axis_segments.axis('off')
     axis_segments.imshow(segmentation)
     axis_segments.set_title("all segments", fontsize=16)
-    black_pixel = np.array([0,0,0])
+    black_pixel = np.array([0,0,0], dtype=np.uint8)
     for (axis_mask, axis_maskedimage), m in zip(axes[1:], models):
         mask = (segmentation == model2id[m])
         axis_mask.axis('off')
@@ -202,6 +204,7 @@ class apply_models():
         super().__init__()
 
         self.load_edited_segmentation = False
+        self.max_threads = 3 # more than this may hog your memory. assume 2GB per thread.
 
         self.path = path
 
@@ -235,11 +238,9 @@ class apply_models():
         return result
 
     def run(self):
-        max_threads = 3 # more than this may hog your memory. assume 2GB per thread.
-
         threads = multiprocessing.cpu_count() - 1
-        if threads > max_threads:
-            threads = max_threads
+        if threads > self.max_threads:
+            threads = self.max_threads
 
         with multiprocessing.Pool(threads, maxtasksperchild=1) as p:
             results = p.map(self.apply_models, self.all_images, chunksize=1)
@@ -260,5 +261,7 @@ class apply_models():
 class reapply_models(apply_models):
     def __init__(self, path):
         super().__init__(path)
+
         self.load_edited_segmentation = True
+        self.max_threads = 6
 
